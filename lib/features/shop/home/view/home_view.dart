d76b2fb4b50801c737724f9927902ee939d45397
Ali Afzal <aliafzal.dev24@gmail.com>
2025-03-30 14:48:38 +0500

    Vertical Product Card added in Home Page

--- lib/features/shop/home/view/home_view.dart
+++ lib/features/shop/home/view/home_view.dart
@@ -18,6 +18,7 @@ class _BodyView extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const HeaderWidget(
               child: Column(
@@ -31,7 +32,9 @@ class _BodyView extends StatelessWidget {
                   SizedBox(
                     height: AppSizes.spaceBtwSection,
                   ),
-                  TypeWithSuffixWidget(),
+                  TypeWithSuffixWidget(
+                    title: 'Popular Categories',
+                  ),
                   SizedBox(
                     height: AppSizes.lg,
                   ),
@@ -42,7 +45,44 @@ class _BodyView extends StatelessWidget {
                 ],
               ),
             ),
-            _CustomSliderWidget(controller: controller),
+            const SizedBox(
+              height: AppSizes.lg,
+            ),
+            CustomSliderWidget(
+              controller: controller,
+            ),
+            const SizedBox(
+              height: AppSizes.lg,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TypeWithSuffixWidget(
+                    title: 'Popular Products',
+                    suffix: 'View All',
+                    onTap: () {},
+                  ),
+                  const SizedBox(
+                    height: AppSizes.lg,
+                  ),
+                  Row(
+                    children: [
+                      VeticalProductCard(
+                        imageUrl: 'assets/images/splash_image.png',
+                        onPressed: () {},
+                      ),
+                      const Spacer(),
+                      VeticalProductCard(
+                        imageUrl: 'assets/images/splash_image.png',
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -50,75 +90,208 @@ class _BodyView extends StatelessWidget {
   }
 }
 
-class _CustomSliderWidget extends StatelessWidget {
-  const _CustomSliderWidget({
-    required this.controller,
+class VeticalProductCard extends StatelessWidget {
+  const VeticalProductCard({
+    super.key,
+    this.imageUrl,
+    this.title,
+    this.subtitle,
+    this.priceRange,
+    this.onPressed,
   });
-
-  final HomeController controller;
+  final String? imageUrl;
+  final String? title;
+  final String? subtitle;
+  final String? priceRange;
+  final VoidCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CarouselSlider(
-          items: const [
-            _RoundedImageWidget(),
-            _RoundedImageWidget(),
-            _RoundedImageWidget()
+    final dark = CustomHelpers.isDark(context);
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: 245,
+        width: MediaQuery.of(context).size.width * 0.44,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            AppSizes.producstImageRadius,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              blurRadius: 50,
+              spreadRadius: 7,
+              offset: const Offset(0, 2),
+            ),
           ],
-          options: CarouselOptions(
-            viewportFraction: 1,
-            onPageChanged: (index, _) {
-              controller.setCurrentIndex(index);
-            },
-          ),
+          color: dark ? Colors.white : Colors.grey,
         ),
-        Obx(
-          () => Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              for (int i = 0; i < 3; i++)
-                CustomCircularContainer(
-                  margin: const EdgeInsets.only(right: 10),
-                  height: 4,
-                  width: 16,
-                  backgroundColor: controller.currentIndex.value == i
-                      ? Colors.green
-                      : Colors.grey,
-                ),
-            ],
-          ),
-        )
-      ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomCircleContainer(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              height: 140,
+              child: Stack(
+                children: [
+                  RoundedImageWidget(
+                    imageUrl: imageUrl ?? '',
+                    applyImageRadius: true,
+                  ),
+                  Positioned(
+                    top: 12,
+                    child: CustomCircleContainer(
+                        radius: AppSizes.sm,
+                        backgroundColor: Colors.orange,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: AppSizes.sm,
+                          vertical: AppSizes.xs,
+                        ),
+                        child: Text(
+                          '25%',
+                          style:
+                              Theme.of(context).textTheme.labelLarge!.copyWith(
+                                    color: Colors.black,
+                                  ),
+                        )),
+                  ),
+                  const Positioned(
+                    top: 0,
+                    right: 0,
+                    child: CustomCircularIcon(
+                      icon: Icon(
+                        Icons.favorite_outline,
+                        color: Colors.red,
+                        size: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: AppSizes.spaceBtwItems / 2,
+            ),
+            //Title,Price,Cart
+
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ProductTitleText(
+                    title: 'Green Nike Air Shoes',
+                  ),
+                  const SizedBox(
+                    height: AppSizes.spaceBtwItems / 2,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Nike',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                      const SizedBox(
+                        width: AppSizes.xs,
+                      ),
+                      const Icon(
+                        Icons.verified,
+                        size: 15,
+                        color: Colors.black,
+                      ),
+                    ],
+                  ),
+                  // const Spacer(),
+                  Row(
+                    children: [
+                      const Text(
+                        '\$122.6',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                      const Spacer(),
+                      Container(
+                        padding: const EdgeInsets.all(1),
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(6),
+                            bottomRight: Radius.circular(16),
+                          ),
+                          color: Colors.black,
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class _RoundedImageWidget extends StatelessWidget {
-  const _RoundedImageWidget();
+class ProductTitleText extends StatelessWidget {
+  const ProductTitleText({
+    super.key,
+    this.title,
+    this.maxLines,
+    this.textAlign,
+    this.style,
+  });
+  final String? title;
+  final int? maxLines;
+  final TextAlign? textAlign;
+  final TextStyle? style;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(
-        AppSizes.defaultSpace,
-      ),
-      child: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(AppSizes.md)),
-        ),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(AppSizes.md),
-          ),
-          child: Image.asset(
-            'assets/images/banner_image1.png',
-            // cacheHeight: 200,
-            // cacheWidth: 400,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.contain,
-          ),
+    return Text(
+      title ?? '',
+      style: style ?? Theme.of(context).textTheme.titleSmall,
+      maxLines: maxLines ?? 2,
+      textAlign: textAlign ?? TextAlign.left,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
+
+class CustomCircularIcon extends StatelessWidget {
+  const CustomCircularIcon({
+    super.key,
+    this.icon,
+    this.onPressed,
+  });
+  final Icon? icon;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0,
+      top: 12,
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          padding: const EdgeInsets.all(4),
+          decoration: BoxDecoration(
+              color: Colors.white.withOpacity(
+                0.9,
+              ),
+              borderRadius: BorderRadius.circular(
+                100,
+              )),
+          child: icon,
         ),
       ),
     );
@@ -128,16 +301,31 @@ class _RoundedImageWidget extends StatelessWidget {
 class TypeWithSuffixWidget extends StatelessWidget {
   const TypeWithSuffixWidget({
     super.key,
+    this.title,
+    this.suffix,
+    this.onTap,
   });
+  final String? title;
+  final String? suffix;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16.0),
-      child: Text(
-        'Popular Categories',
-        style: Theme.of(context).textTheme.headlineSmall,
-      ),
+    return Row(
+      children: [
+        Text(
+          title ?? '',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        const Spacer(),
+        GestureDetector(
+          onTap: onTap,
+          child: Text(
+            suffix ?? '',
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+        ),
+      ],
     );
   }
 }
@@ -189,14 +377,14 @@ class HeaderWidget extends StatelessWidget {
                 Positioned(
                   top: -150,
                   right: -250,
-                  child: CustomCircularContainer(
+                  child: CustomCircleContainer(
                     backgroundColor: Colors.white.withOpacity(0.3),
                   ),
                 ),
                 Positioned(
                   top: 100,
                   right: -300,
-                  child: CustomCircularContainer(
+                  child: CustomCircleContainer(
                     backgroundColor: Colors.white.withOpacity(0.3),
                   ),
                 ),
@@ -210,22 +398,24 @@ class HeaderWidget extends StatelessWidget {
   }
 }
 
-class CustomCircularContainer extends StatelessWidget {
-  const CustomCircularContainer({
+class CustomCircleContainer extends StatelessWidget {
+  const CustomCircleContainer({
     super.key,
-    this.width = 400,
-    this.height = 400,
-    this.radius = 400,
-    this.padding = 0,
+    this.width,
+    this.height,
+    this.radius = AppSizes.cardRadiusLg,
+    this.padding,
     this.backgroundColor,
     this.margin,
+    this.child,
   });
   final double? width;
   final double? height;
   final double radius;
-  final double padding;
+  final EdgeInsetsGeometry? padding;
   final Color? backgroundColor;
   final EdgeInsetsGeometry? margin;
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
@@ -233,11 +423,12 @@ class CustomCircularContainer extends StatelessWidget {
       margin: margin,
       width: width,
       height: height,
-      padding: EdgeInsets.all(padding),
+      padding: padding,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(radius),
         color: backgroundColor,
       ),
+      child: child,
     );
   }
 }
